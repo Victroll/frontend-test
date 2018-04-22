e6f945e5cf1a39e8146a9ed200f420a810aa4bd7
Víctor Gutiérrez <victorgutierrez1988@gmail.com>
2018-04-22 18:01:25 +0200

    Added listener to the markers

--- src/containers/MapView.jsx
+++ src/containers/MapView.jsx
@@ -6,6 +6,7 @@ class MapView extends React.Component {
         super(props);
 
         this.refreshMarkers = this.refreshMarkers.bind(this);
+        this.selectFav = this.selectFav.bind(this);
 
         this.state = {
             map: null
@@ -35,34 +36,50 @@ class MapView extends React.Component {
     refreshMarkers(cityName, refreshThis = null) {
         // Remove previous markers
         this.markers.forEach((marker) => marker.setMap(null));
-    
+
         // Create new markers
         const newMarkers = [];
-    
+
         if (refreshThis)
-          refreshThis.forEach((data) => {
+            refreshThis.forEach((data) => {
             const marker = new window.google.maps.Marker({
-              map: this.state.map,
-              position: {lat: data.latitude, lng: data.longitude},
-              icon: data.fav ? './images/heart.svg' : ''
+                map: this.state.map,
+                position: {lat: data.latitude, lng: data.longitude},
+                icon: data.fav ? './images/heart.svg' : ''
             });
             marker.addListener('click', (e) => this.selectFav(data));
             newMarkers.push(marker);
-          });
+            });
         else
-          this.props.dataByCity[cityName].forEach((data) => {
+            this.props.dataByCity[cityName].forEach((data) => {
             const marker = new window.google.maps.Marker({
-              map: this.state.map,
-              animation: window.google.maps.Animation.DROP,
-              position: {lat: data.latitude, lng: data.longitude},
-              icon: data.fav ? './images/heart.svg' : ''
+                map: this.state.map,
+                animation: window.google.maps.Animation.DROP,
+                position: {lat: data.latitude, lng: data.longitude},
+                icon: data.fav ? './images/heart.svg' : ''
             });
             marker.addListener('click', (e) => this.selectFav(data));
             newMarkers.push(marker);
-          });
+            });
 
         this.markers = newMarkers;
-      }
+    }
+
+    selectFav(data) {
+        const newFavData = this.props.dataByCity[data.city_name].map((station) => {
+            if (station.id === data.id)
+            return {...station, fav: !station.fav}
+            return station;
+        });
+
+        this.refreshMarkers(data.city_name, newFavData);
+
+        this.setState({...this.state, 
+            dataByCity: {...this.state.dataByCity,
+            [data.city_name]: newFavData
+            }
+        });
+    }
 
     render() {
         return (
