5e368bda24413494ba47afa33d63f9c5fc506cdb
Víctor Gutiérrez <victorgutierrez1988@gmail.com>
2018-04-22 14:07:59 +0200

    Data fetched from url and stored in the main component

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,6 +1,36 @@
 import React from 'react';
+import { get } from 'axios';
+
+const DATA_FETCH_URL =
+'https://gist.githubusercontent.com/inakivb/943ed6b3a8bcc667c1e1147b7591e32f/raw/355b2d67aaea30fd322c7d1e1a8660480609d67a/stations.json';
 
 class App extends React.Component {
+    constructor() {
+        super();
+
+        this.fetchData = this.fetchData.bind(this);
+
+        this.state = {
+            stationsData: []
+        };
+
+        // Fetch the stations data
+        this.fetchData();
+        
+    }
+
+    fetchData() {
+        get(DATA_FETCH_URL)
+            .then((response) => {
+                this.setState({...this.state,
+                    stationsData: response.data
+                });
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }
+
     render() {
         return (
             <div>
